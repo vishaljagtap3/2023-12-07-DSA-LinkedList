37ce17ebcb575940ad310c42c9841cf1cf80a2a7
vishal <vishal.jagtap@bitcode.in>
2023-12-13 18:22:52 +0530

    insert display count discussion regarding count in context with time complexity - Recursion in detail - Display linked list in reverse order using rec and non-rec functions - reversing the linked list - inserting by position - deleting by position and value

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -15,18 +15,18 @@ public class LinkedList {
 
     public boolean insert(int data) {
         Node newNode = new Node(data);
-        if(newNode == null) {
+        if (newNode == null) {
             return false;
         }
 
-        if(head == null) {
+        if (head == null) {
             head = newNode;
             count++;
             return true;
         }
 
         Node last = head;
-        while(last.getNext() != null) {
+        while (last.getNext() != null) {
             last = last.getNext();
         }
 
@@ -38,7 +38,7 @@ public class LinkedList {
 
     public void display() {
         Node temp = head;
-        while(temp != null) {
+        while (temp != null) {
             System.out.print(temp.getData() + " ");
             temp = temp.getNext();
         }
@@ -49,7 +49,7 @@ public class LinkedList {
         Node temp = head;
         int count = 0;
 
-        while(temp != null) {
+        while (temp != null) {
             count++;
             temp = temp.getNext();
         }
@@ -65,19 +65,19 @@ public class LinkedList {
         Stack<Node> stack = new Stack<>();
         Node temp = head;
 
-        while(temp != null) {
+        while (temp != null) {
             stack.push(temp);
             temp = temp.getNext();
         }
 
-        while(!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
             System.out.print(stack.pop().getData() + " ");
         }
         System.out.println();
     }
 
     public void displayRev(Node head) {
-        if(head == null) {
+        if (head == null) {
             return;
         }
 
@@ -86,14 +86,14 @@ public class LinkedList {
     }
 
     public void reverse() {
-        if(head == null) {
+        if (head == null) {
             return;
         }
 
         Node n1 = head, n2 = head.getNext();
         Node n3;
 
-        while(n2 != null) {
+        while (n2 != null) {
             n3 = n2.getNext();
             n2.setNext(n1);
             n1 = n2;
@@ -102,6 +102,94 @@ public class LinkedList {
 
         head.setNext(null);
         head = n1;
+    }
+
+    public boolean insert(int data, int position) {
+        if (position < 1 || position > (count + 1)) {
+            return false;
+        }
+
+        Node newNode = new Node(data);
+        if (newNode == null) {
+            return false;
+        }
+
+        //if position is 1
+        if (position == 1) {
+            newNode.setNext(head);
+            head = newNode;
+            count++;
+            return true;
+        }
+
+        //if the position is other than 1
+        //locate prev node i.e. the node at position - 1
+        Node prev = head;
+        for (int i = 1; i < position - 1; i++) {
+            prev = prev.getNext();
+            //if prev becomes null, means that position is invalid
+            if (prev == null) {
+                return false;
+            }
+        }
+
+        //link new node into the list
+        newNode.setNext(prev.getNext());
+        prev.setNext(newNode);
+        count++;
+        return true;
+    }
+
+    public boolean deleteByPosition(int position) {
+        if (position < 1 || head == null) {
+            return false;
+        }
+
+        if (position == 1) {
+            head = head.getNext();
+            return true;
+        }
+
+        //locate prev node
+        Node prev = head;
+        for (int i = 1; i < position - 1; i++) {
+            prev = prev.getNext();
+            if (prev == null) {
+                return false;
+            }
+        }
+
+        Node del = prev.getNext();
+        if (del == null) {
+            return false;
+        }
+        prev.setNext(del.getNext());
+
+        return true;
+    }
+
+    public boolean deleteByValue(int data) {
+        if (head == null) {
+            return false;
+        }
+
+        if (data == head.getData()) {
+            head = head.getNext();
+            return true;
+        }
+
+        Node prev = head, del = head;
+
+        while(del.getData() != data) {
+            prev = del;
+            del = del.getNext();
+            if(del == null) {
+                return false;
+            }
+        }
+
+        prev.setNext(del.getNext());
+        return true;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,37 @@ public class Main {
         l1.insert(30);
         l1.insert(40);
 
-        System.out.println("size: " + l1.size());
+        l1.display();
+
+        l1.insert(50, 1);
+        l1.display();
+        l1.insert(60, 4);
+        l1.display();
+        l1.insert(70, 7);
+        l1.display();
+        System.out.println( l1.insert(80, 9) );
+        l1.display();
+
+        l1.deleteByValue(50);
+        l1.display();
+        l1.deleteByValue(60);
+        l1.display();
+        l1.deleteByValue(70);
+        l1.display();
+        l1.deleteByValue(70);
+        l1.display();
+
+        /*l1.deleteByPosition(1);
+        l1.display();
+        l1.deleteByPosition(4);
+        l1.display();
+        l1.deleteByPosition(5);
+        l1.display();
+        l1.deleteByPosition(5);
+        l1.display();*/
+
+
+        /*System.out.println("size: " + l1.size());
 
         l1.display();
         l1.displayRev();
@@ -25,7 +55,7 @@ public class Main {
         l1.reverse();
         l1.display();
         l1.displayRev();
-
+        */
 
     }
 }
